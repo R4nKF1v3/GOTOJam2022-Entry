b4294da1267f918ddc675b1dedf6f8c9b5173296
R4nKF1v3 <nahumoscatelli@gmail.com>
2022-11-21 23:43:05 -0300

    Last commit

--- screens/MainMenuScreen.gd
+++ screens/MainMenuScreen.gd
@@ -23,3 +23,6 @@ func _play_exit_animation()->void:
 func _on_StartButton_button_up() -> void:
 	_change_screen(GameState.SCREENS.LOADING, GameState.SCREENS.GAME)
 
+
+func _on_ReturnButton_button_up() -> void:
+	get_tree().quit()
